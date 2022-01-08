ce7c29000f4647c52c9af82ece2b4fd4379df8ef
Marcosta23 <mpc2073@yahoo.com.br>
2022-01-08 14:06:40 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Jogo da Velha
+
+## Atividade proposta por Vitor Ruschoni, da DIO.
+
+- Por enquanto, sem diferenciais (08/01/2022);
--- script.js
+++ script.js
@@ -1,16 +1,134 @@
 
-var player = null;
+var player, winner = null;
 var selectPlayer = document.getElementById('select-player');
+//var squares = document.getElementsByClassName('square');
+var selectWinner = document.getElementById('winner-player');
 
 changePlayer('X');
-function chooseSquare (id){
-    var square = document.getElementById(id);
 
+function chooseSquare (id){
+if(winner !== null){
+    return
+}
+
+    var square = document.getElementById(id);
+     if(square.innerHTML !== '-'){
+        return;
+    }
     square.innerHTML = player;
     square.style.color = '#000';
-}
+
+    
+        if(player === 'X'){
+            player = 'O'
+
+        }else{
+            player = 'X'
+        }
+        changePlayer(player);
+        checked();
+    }
 
 function changePlayer(valor) {
     player = valor;
     selectPlayer.innerHTML = player;
+
+}
+
+function checked(){
+    var sq1 = document.getElementById(1);
+    var sq2 = document.getElementById(2);
+    var sq3 = document.getElementById(3);
+    var sq4 = document.getElementById(4);
+    var sq5 = document.getElementById(5);
+    var sq6 = document.getElementById(6);
+    var sq7 = document.getElementById(7);
+    var sq8 = document.getElementById(8);
+    var sq9 = document.getElementById(9);
+
+    if (checkSeq(sq1, sq2, sq3)) {
+        changeColorSquare(sq1, sq2, sq3);
+        changeWinner(sq1);
+        return;
+    }
+
+    if (checkSeq(sq4, sq5, sq6)) {
+        changeColorSquare(sq4, sq5, sq6);
+        changeWinner(sq4);
+        return;
+    }
+
+    if (checkSeq(sq7, sq8, sq9)) {
+        changeColorSquare(sq7, sq8, sq9);
+        changeWinner(sq7);
+        return;
+    }
+
+    if (checkSeq(sq1, sq4, sq7)) {
+        changeColorSquare(sq1, sq4, sq7);
+        changeWinner(sq1);
+        return;
+    }
+
+    if (checkSeq(sq2, sq5, sq8)) {
+        changeColorSquare(sq2, sq5, sq8);
+        changeWinner(sq2);
+        return;
+    }
+
+    if (checkSeq(sq3, sq6, sq9)) {
+        changeColorSquare(sq3, sq6, sq9);
+        changeWinner(sq3);
+        return;
+    }
+
+    if (checkSeq(sq1, sq5, sq9)) {
+        changeColorSquare(sq1, sq5, sq9);
+        changeWinner(sq1);
+        return;
+    }
+
+    if (checkSeq(sq3, sq5, sq7)) {
+        changeColorSquare(sq3, sq5, sq7);
+        changeWinner(sq3);
+    }
+}
+
+
+function changeWinner(square) {
+    winner = square.innerHTML;
+    selectWinner.innerHTML= winner;
+
+}
+
+function changeColorSquare(sq1,sq2,sq3){
+    sq1.style.background = '#0f0';
+    sq2.style.background = '#0f0';
+    sq3.style.background = '#0f0';
+}
+
+function checkSeq(sq1, sq2, sq3) {
+    var compare = false;
+
+    if (sq1.innerHTML !== '-' && sq1.innerHTML === sq2.innerHTML && sq2.innerHTML === sq3.innerHTML) {
+        compare = true;
+    }
+
+    return compare;
+}
+
+function reStart(){
+    console.log("passei2")
+    winner = null;
+    selectWinner.innerHTML = '';
+
+    for (var i = 1; i <= 9; i++) {
+        var square = document.getElementById(i);
+        square.style.background = '#eee';
+        square.style.color = '#eee';
+        square.innerHTML = '-';
+        console.log("passei1")
+    }
+
+    changePlayer('X');
 }
